1621a43b2f98e6b3e3d3472cf5c61318bc9829dc
Chris Ladden <49324111+cladden77@users.noreply.github.com>
2024-07-10 10:51:52 -0500

    created user request

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,14 @@
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useCreateMyUser } from "../api/MyUserApi";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const auth0ProviderWithNavigate = ({ children }: Props) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { createUser } = useCreateMyUser();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -14,7 +18,9 @@ const auth0ProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("USER", user);
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
 
   return (
