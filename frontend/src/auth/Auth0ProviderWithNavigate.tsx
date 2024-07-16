cae5d112baaef6c70d505f5238ce0f7a7d8b731e
Chris Ladden <49324111+cladden77@users.noreply.github.com>
2024-07-16 09:37:46 -0500

    removed unused classes

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -16,7 +16,7 @@ const auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("Unable to initialize auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
+  const onRedirectCallback = () => {
     navigate("/auth-callback");
   };
 
